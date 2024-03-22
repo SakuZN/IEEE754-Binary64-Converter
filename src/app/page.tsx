bdecb95a0a1ff32f9892154435eecf4f7e93f018
ylsewlys <129646270+ylsewlys@users.noreply.github.com>
2024-03-22 16:55:57 +0800

    style: title responsiveness
    Co-authored-by: Zach Noche <106810417+sakuzn@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import BinaryViz from "@/app/components/BinaryViz";
 export default function Home() {
   return (
     <div className="flex flex-col space-y-5">
-      <h1 className="flex justify-center text-5xl font-semibold">
+      <h1 className="flex justify-center text-center text-5xl mq1350:text-3xl font-semibold">
         IEEE 754 Double Precision Converter
       </h1>
       <BinaryViz />
